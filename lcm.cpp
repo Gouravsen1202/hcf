ae4e12b149c106ef5bec2eb372b14fe9ed97e68b
Gouravsen1202 <gouravsen757@gmail.com>
2024-11-18 18:50:58 +0530

    first commit

---
+++ lcm.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int hcf;
+	int larger=7;
+	int greater=9;
+	int i;
+ 
+
+	for(int i=larger; ; i=i+larger){
+	
+	  if(i%larger==0 && i%greater==0){
+	hcf=i;
+	 
+		
+	
+		break;
+		}
+}
+	cout<< hcf;
+}
+
